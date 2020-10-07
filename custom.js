3d8c03f5005242595375279fbc3cd6618448247a
Mitchell <mitchell.gaertner@gmail.com>
2020-10-07 17:15:20 -0600

    local json querying

--- custom.js
+++ custom.js
@@ -3,3 +3,15 @@ var util = {};
 util.init = () => {
 	console.log("hello");
 }
+
+util.readJson = (file, callback) => {
+	var rawFile = new XMLHttpRequest();
+	rawFile.overrideMimeType("application/json");
+	rawFile.open("GET", file, true);
+	rawFile.onreadystatechange = () => {
+		if (rawFile.readyState === 4 && rawFile.status === "200"){
+			callback(rawFile.responseText);
+		}
+	}
+	rawFile.send(null);
+};
