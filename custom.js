e461e06aeaa813b2fc60334f02da9ce74a7d7aa6
Mitchell <mitchell.gaertner@gmail.com>
2020-10-07 19:41:53 -0600

    implemented shared function

--- custom.js
+++ custom.js
@@ -37,5 +37,55 @@ util.addRow = (table, item, index) => {
 
 util.process = (state) => {
 	let decks = state.decks;
-	console.log(decks);
+	let processed = {};
+	processed.allCards = util.getAll(decks);
+	processed.shared = util.getShared(decks, processed.allCards);
+	return processed;
+}
+
+// function processDecks(decks) {
+// 	let arr = [];
+// 	decks.forEach((item) => {
+// 		let cards = util.getCards(item);
+// 		cards.forEach((card) => {
+
+// 		};
+// 	});
+// }
+
+util.getAll = (decks) => {
+	let all = new Set();
+	decks.forEach(deck => {
+		util.getCards(deck).forEach(card => {
+			all.add(card);
+		})
+	})
+	return Array.from(all);
+};
+
+//Excludes Basics
+util.getCards = (deck) => {
+	let cards = [];
+	deck.entries.nonlands.forEach(item => {
+		if (!item?.card_digest?.type_line.includes("Basic")) cards.push(item?.card_digest?.name);
+	});
+	deck.entries.lands.forEach(item => {
+		if (!item?.card_digest?.type_line.includes("Basic")) cards.push(item?.card_digest?.name);
+	});
+	return cards;
+}
+
+util.getShared = (decks, fullList) => {
+	let sharedList = [];
+	fullList.forEach(card => {
+		let shared = true;
+		for (let i = 0; i < decks.length; i++){
+			let cards = util.getCards(decks[i]);
+			if (!cards.includes(card)) shared = false;
+		}
+		if (shared){
+			sharedList.push(card);
+		}
+	})
+	return sharedList;
 }
